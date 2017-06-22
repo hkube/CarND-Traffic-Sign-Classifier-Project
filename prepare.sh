7b2dfaacc93a2148199ea3656f135a3bb48c881d
Harald Kube <harald.kube@gmx.de>
2017-06-22 22:02:17 +0200

    Add script to prepare the data

---
+++ prepare.sh
@@ -0,0 +1,8 @@
+#!/bin/bin
+
+wget https://d17h27t6h515a5.cloudfront.net/topher/2017/February/5898cd6f_traffic-signs-data/traffic-signs-data.zip
+mkdir traffic-signs-data
+cd traffic-signs-data
+unzip -x ../traffic-signs-data.zip
+cd ..
+
